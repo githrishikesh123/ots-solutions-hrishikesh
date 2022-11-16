044199ab00406e8f08f63141fb4f9643c9f83a89
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2022-11-16 15:31:27 +0530

    project update

--- Projects/BMIcalculator/BMI.js
+++ Projects/BMIcalculator/BMI.js
@@ -1,6 +1,6 @@
 let bmiQueries=[];
 function submitBtn(){
-    
+    // ----- Input values in varibales 
     var height = document.getElementById("heightInput").value;
     console.log("height is : ", height)
     let weight = document.getElementById("weightInput").value;
@@ -9,15 +9,33 @@ function submitBtn(){
     let bmi = ((weight/((height)**2)))
     let bmiRes = bmi.toFixed(2)
     document.getElementById("bmires").innerHTML=bmiRes;
+    // ------- Push values in array in store in bmiQueries
     bmiQueries.push(bmiRes)
     console.log(bmiQueries)
     document.getElementById("bmiQueries").innerHTML=bmiQueries;
-    if(height<=0 || weight<=0){
-        alert("Enter Valid Number");
-    }
+    // ---------- Check for No inputs 
+
+    // --------- Shift entries in array 
     if(bmiQueries.length>=10){
         bmiQueries.shift();
     }
+    // ------- clear input field after calculation ----
     document.getElementById("heightInput").value="";
     document.getElementById("weightInput").value="";
+    if(height<=0 || weight<=0){
+        alert("Enter Valid Number");
+        document.getElementById("bmires").innerHTML=bmiRes;
+    document.getElementById("bmiQueries").innerHTML=bmiQueries;
+    }
+    // ----------- Result Cases ----------
+    if(bmiRes<18.5){
+        alert("You Are underweight")
+    }else if(bmiRes>18.5 && bmiRes<24.9){
+        alert("You've Healthy Weight")
+    }else if(bmiRes>25 && bmiRes<30){
+        alert("You are Overweight")
+    }
+    else if(bmiRes>30){
+        alert("You are in Obesity state")
+    }
 }
