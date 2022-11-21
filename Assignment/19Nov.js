2f043bb85bbc620656099d0e34821f53e01cbff5
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2022-11-21 20:21:06 +0530

    assignment updates

--- Assignment/19Nov.js
+++ Assignment/19Nov.js
@@ -1,3 +1,4 @@
+// ================================ 1 =======================
 // ===== Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) =============
 /*
 Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude value (lng) (these are GPS coordinates, examples are below).
@@ -9,6 +10,7 @@ Note: We have geoLocation method for getting the current coordinates:
 https://www.w3schools.com/jsref/prop_nav_geolocation.asp
 */
 
+/*
 const whereAmI = async (lat,long) => {
     try {
       const data = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`);
@@ -21,34 +23,41 @@ const whereAmI = async (lat,long) => {
   };
   whereAmI(29.94569,78.164246);
 
+  */
+//   ----------------------------------------
+
+  const whereAmI2 = (lat,long) => {
+    fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=en`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((result) => {
+        console.log(`You Are In ${result.city}`)
+      })
+      .catch((error) => console.log(error, "Error occured"));
+  };
+  whereAmI2(11.016010,76.970306);
 
 
-//   =======================
+
+//   ======================= 2 ======================
+//=======================  Get req ========================
+
+/*
 const getData = async () => {
   await axios.get("https://reqres.in/api/users").then((res) => {
+    
     console.log(res.data);
+    let user = res.data;
+    console.log(user.data[3])
   });
 };
 getData();
-
-//  https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=lat&longitude=long&localityLanguage=en
-
-// https://reqres.in/api/users
+*/
 
 
-// const getData = async () => {
-//   try {
-//     const data = await fetch(`https://reqres.in/api/users`);
-//     const myData = await data.json();
-//     console.log(myData);
-//     // console.log(myData.data[2]);
-//   } catch (err) {
-//     console.log(err, "Error Occured...");
-//   }
-// };
 
-
-// ============ POST req , New User ================
+// ============ POST req , Add New User in (https://reqres.in/api/users) ================
 
 // const myData = ()=>{
 //     axios.post(`https://reqres.in/api/users`,
@@ -66,5 +75,45 @@ getData();
 //     })
 // }
 // myData()
+
+
+
+
+
+
+
+
+
+
+
+
+//  https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=lat&longitude=long&localityLanguage=en
+
+// https://reqres.in/api/users
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =================== Raw Work ===============
+// const getData = async () => {
+//   try {
+//     const data = await fetch(`https://reqres.in/api/users`);
+//     const myData = await data.json();
+//     console.log(myData);
+//     // console.log(myData.data[2]);
+//   } catch (err) {
+//     console.log(err, "Error Occured...");
+//   }
+// };
+
 
 
