0071dbf020591944ee10049caa1178a1b7f910ec
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2022-11-25 11:32:28 +0530

    assignment and project  updates

--- Projects/currentTime/index.js
+++ Projects/currentTime/index.js
@@ -4,12 +4,14 @@ setInterval(function () {
 
   var minutes = hours.getMinutes();
   var seconds = hours.getSeconds();
-  console.log(seconds)
+  console.log(seconds);
 
   if (hour > 12) {
     hour = hour - 12;
-    document.getElementById("hourValue").innerHTML = hour;
-    document.getElementById("minuteValue").innerHTML = minutes;
-    document.getElementById("secondValue").innerHTML = seconds;
+    document.querySelector(".dayEvent").innerHTML="PM";
   }
+
+  document.getElementById("hourValue").innerHTML = hour;
+  document.getElementById("minuteValue").innerHTML = minutes;
+  document.getElementById("secondValue").innerHTML = seconds;
 }, 1000);
