a4e7541ed942f65b6f38883593be56ec68d868c9
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2022-11-28 11:43:47 +0530

    assignment and project  updates

--- Projects/ToDoList/List.js
+++ Projects/ToDoList/List.js
@@ -2,14 +2,15 @@ var addButton = document.getElementById("addBtn");
 var store = [];
 
 addButton.addEventListener("click", () => {
+
   var inputVal = document.getElementById("inputField").value;
   console.log(inputVal.length);
 
   if (inputVal.length) {
     console.log(inputVal.length);
-    const createList = document.createElement("li");
-    createList.innerHTML = inputVal;
-    document.getElementById("list").appendChild(createList);
+    const createList = document.createElement("li"); // create li
+    createList.innerHTML = inputVal;                  // inputval to createlist i.e in li 
+    document.getElementById("list").appendChild(createList); // append createlist
 
     let createBtn = document.createElement("button");
     createBtn.innerHTML = "Delete";
@@ -25,9 +26,10 @@ addButton.addEventListener("click", () => {
 
   for (var i = 0; i < store.length; i++) {}
   localStorage.setItem(i, inputVal);
+  document.getElementById("inputField").value=""
 });
 
-function hi() {
+function percistFunc() {
   for (i = 0; i <= localStorage.length; i++) {
     var get = localStorage.getItem(i);
     const createList2 = document.createElement("li");
