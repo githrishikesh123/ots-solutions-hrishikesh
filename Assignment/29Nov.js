0d50c30559e6e3cc0975ccd0b710fcc978e2d07c
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2022-11-29 23:26:25 +0530

    assignment and project  updates

--- Assignment/29Nov.js
+++ Assignment/29Nov.js
@@ -1,3 +1,10 @@
+// Event Bubbling===>
+//    he event movement begins from the target to the outermost element.
+//    It means when the user click on the button, the click event flows in this order from bottom to top.
+//    event bubbling is used where the event handlers are invoked when-
+//    one element is nested on to the other element and are part of the same event.
+
+/*
 document.getElementById("outmost").addEventListener("click", () => {
   document.getElementById("outmost").style.backgroundColor = "Teal";
 });
@@ -8,6 +15,36 @@ document.getElementById("outer").addEventListener("click", () => {
 document.getElementById("paraOne").addEventListener("click", () => {
   document.getElementById("paraOne").style.backgroundColor = "Chartreuse";
 });
+
+*/
+
+// Event Capturing=====>
+// Netscape Browser was the first to introduce the concept of Event Capturing. 
+// Event Capturing is opposite to event bubbling, where in event capturing, 
+///// an event moves from the outermost element to the target.
+
+let outmost = document.getElementById("outmost");  
+let outer = document.getElementById("outer");  
+let paraOne = document.getElementById("paraOne");  
+
+// outmost.addEventListener('click', function(){  
+//   console.log("Outmost is invoked");  
+// },true);  
+// outer.addEventListener('click', function(){  
+//   console.log("Outer is invoked");  
+// });  
+// paraOne.addEventListener('click', function(){  
+//   console.log("ParaOne is invoked");  
+// }); 
+
+for(let elem of document.getElementById('*')) {
+  elem.addEventListener("click", e => console.log("Capturing:", elem.tagName), true);
+}
+
+
+
+
+
 
 
 
