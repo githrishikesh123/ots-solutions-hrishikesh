7115523d09a4e8e3d649e5e8645201323d269619
githrishikesh123 <hrishikesh.bhagat@otssolutions.com>
2022-12-06 11:50:52 +0530

    ReactJS updates

---
+++ Assignment/5Dec/ToDoApp/todoapp/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import ButtonOne from "./Components/ButtonOne";
+
+function App() {
+  return (
+    <div className="App">
+      <div>React Form</div>
+      <div className="todo-main">
+        <ButtonOne />
+      </div>
+      <div className="todo-content">todo content here</div>
+    </div>
+  );
+}
+export default App;
---
+++ Assignment/5Dec/ToDoApp/todoapp/src/Components/InputFieldComponent.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const InputFieldComponent = () => {
+
+
+  return (
+    <>
+      <div className="inputfield-main">
+        <input className="inputField" id="myInput" placeholder="enter data" type={"text"} autoComplete="off"></input>
+      </div>
+    </>
+  );
+};
+export default InputFieldComponent;
